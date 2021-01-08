f1f854dcaf917ae923595a8431263ddd8dede2da
raihan <raihanmuhd97@gmail.com>
2021-01-09 02:34:48 +0800

    added visualizer function

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from analyzer import analyze_video
+from hnrdataviz.visualizer import visualizer
+
 app = Flask(__name__)
 cors = CORS(app)
 
@@ -14,4 +16,6 @@ def analyze():
     # test_url = "https://media.istockphoto.com/videos/group-of-six-friends-saying-cheers-over-video-chat-video-id1221689160"
     data = request.get_json()
     result = analyze_video(data['url'])
+    dict_htmls = visualizer(result)
+    return dict_htmls
 
