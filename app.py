62ed4f560ccf10eeafb16611df60a5361ab6c241
abhinav <ambhinav@yahoo.com>
2021-01-08 15:44:31 +0800

    add welcome msg

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, jsonify, request
 from analyzer import analyze_video
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+    msg = "Welcome to Emotional Intelligenze server!"
+    return msg;
+
 @app.route('/api/analyze', methods=['POST'])
 def analyze():
     # test_url = "https://media.istockphoto.com/videos/group-of-six-friends-saying-cheers-over-video-chat-video-id1221689160"
